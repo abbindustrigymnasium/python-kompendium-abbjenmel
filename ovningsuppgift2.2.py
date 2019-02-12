2b5ff29bd29f47697a995c55843dea1a0a595740
abbjenmel <43954737+abbjenmel@users.noreply.github.com>
2019-02-12 16:32:58 +0100

    Delete ovningsuppgift2.2.py

--- ovningsuppgift2.2.py
+++
@@ -1,5 +0,0 @@
-print("Pick a float")#skriver ut stringen pick a float
-number = input()#ger variabeln number värdet från input av användaren i formatet string
-number = float(number)#konverterar variabeln number till en float
-number = int(number)#konverterar variabeln number till en integer
-print("The closest whole number to your float is "+ str(number))#skriver ut stringen och variabeln number i string format
