4a1bb9c2084e69c5fdbe28cd9dc1b66eccd45cee
abbjenmel <43954737+abbjenmel@users.noreply.github.com>
2019-02-12 16:33:04 +0100

    Delete ovningsuppgift2.3.py

--- ovningsuppgift2.3.py
+++
@@ -1,5 +0,0 @@
-print("Hello, what is your firstname?")#skriver ut frågan i string format
-firstname = input()#sparar inputet från användaren i variabeln firstname i formatet string
-print("What is your lastname?")#skriver ut frågan i string format
-lastname = input()#sparar inputet från användaren i variabeln lastname i formatet string
-print("Nice to meet you " + firstname + " " + lastname + "!")#skriver ut en fras och värdena från firstname och lastname
